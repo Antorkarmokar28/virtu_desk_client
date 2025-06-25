94ae6724995f6e7e235ef18f3ee054afb326c28b
Antorkarmokar28 <antorkarmokar28@gmail.com>
2025-06-25 18:21:31 +0600

    Some code change

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -3,7 +3,6 @@ import Hero from "@/components/modules/hero/Hero";
 const HomePage = () => {
   return (
     <div>
-      <h1>This is a Home page</h1>
       <Hero></Hero>
     </div>
   );
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+const SignupPage = () => {
+  return (
+    <div>
+      <h1>This is a signup page</h1>
+    </div>
+  );
+};
+
+export default SignupPage;
